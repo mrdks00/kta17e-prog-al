27a5ae3d22908968c3be64d436297eed53543735
mrdks00 <33267841+mrdks00@users.noreply.github.com>
2017-11-17 15:10:21 +0200

    tk

---
+++ src/04 exercises/04 exercises/tunnikontroll/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tunnikontroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Taco Oalenque´");
+            Console.WriteLine("1200 Main ST.");
+            Console.WriteLine();
+
+            Console.WriteLine("----------------------------------------------------------");
+
+            while (true)
+            {
+                Console.Write("Enter price of food item [-1 to quit]: ")
+                    string[] input = Console.ReadLine();
+                int sisestus = int.Parse(input);
+                string[] sisestus = new string[];
+
+
+                    
+                    
+                int hind = int.Parse(input);
+                Console.WriteLine();
+
+                int toquit = -1;
+
+                if (hind > toquit)
+
+                string[] hinnad = new string[];
+
+                hinnad[0] = "";
+
+
+                if (hind == toquit)
+                {
+                    Console.WriteLine("----------------------------------------------------------");
+                    break;
+                }
+            }
+
+
+            Console.Write("Enter price of food item [-1 to quit]: ")
+        
+
+               // Print Restaurant Receipt
+//Program which generates a receipt for a restaurant. User can enter N + 1 individual items until - 1 is entered to signify that there are no more items.Then, based on entered items, the subtotal, 15 % gratuity and total are calculated and displayed.
+
+
+        }
+    }
+}
