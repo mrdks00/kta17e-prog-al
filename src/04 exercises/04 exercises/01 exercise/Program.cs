8396cfb3287d3492bed4dc2a389662e7ac38d1cf
mrdks00 <33309474+TomAbel@users.noreply.github.com>
2017-11-17 13:58:14 +0200

    jälle üks harjutus

---
+++ src/04 exercises/04 exercises/01 exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paris keeruline on teise klaviatuuriga");
+            Console.WriteLine("...");
+
+        }
+    }
+}
