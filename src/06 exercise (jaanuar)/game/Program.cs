7e152935d729a2fd5fe91b4c3d7d8187561a3e8b
mrdks27 <33267841+mrdks00@users.noreply.github.com>
2018-02-11 15:36:19 +0200

    k

--- src/06 exercise (jaanuar)/game/Program.cs
+++ src/06 exercise (jaanuar)/game/Program.cs
@@ -1,10 +1,104 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace game
+
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] pildid = new string[7] {
+            "      _______\n     |/      |\n     |      (_)\n     |      \\|/\n     |       |\n     |      / \\\n     |\n    _|___\n",
+            " \n             \n            (_)\n            \\|/\n             |\n            / \n      \n\n",
+            " \n             \n            (_)\n            \\|/\n             |\n            \n      \n\n",
+            " \n             \n            (_)\n            \\ /\n             \n            \n      \n\n",
+            " \n             \n            (_)\n            \\ \n             \n            \n      \n\n" +
+            "",
+            " \n             \n            (_)\n             \n            \n      \n   \n\n",
+            " \n             \n            \n             \n            \n      \n   \n\n",
+        };
+
+        string arvatav;
+        string kuvatav = ""; //osaliselt ära arvatud sõna, mille me ekraanile kuvame
+        int luger = 0;
+        string pakuti;
+        int elud = 6;
+        Random random = new Random();
+
+        string[] words = File.ReadAllLines("./dictionary.txt");
+        /*new String[46] {"followed", "provided", "although", "happened", "question",
+        "American", "increase", "received", "anything", "industry", "religion", "building",
+        "interest", "remember", "business", "involved", "required", "children", "national",
+        "services", "complete", "organize", "southern","consider", "personal", "standard",
+        "continue", "planning", "strength", "couldn't", "position", "students","decision",
+        "possible", "suddenly", "directly", "pressure", "thinking", "district", "probably",
+        "together","economic", "problems", "training", "evidence", "programs"};*/
+
+        arvatav = words[random.Next(words.Length)].ToUpper();
+
+        do
+        {
+            kuvatav += "-";
+            luger++;
+        }
+        while (luger < arvatav.Length);
+
+        while (true)
+        {
+            Console.Clear();
+            Console.Write(pildid[elud]);
+            Console.Write("       " + kuvatav + "\n      Guess: ");
+            pakuti = Console.ReadLine().ToUpper();
+
+
+            if (pakuti.Length < 1)
+            {
+                continue;
+            }
+            if (arvatav.IndexOf(pakuti) > -1)
+            {
+                foreach (Match match in Regex.Matches(arvatav, pakuti))
+                {
+                    char[] ajutine = kuvatav.ToCharArray();
+                    ajutine[match.Index] = match.Value[0];
+                    kuvatav = new String(ajutine);
+                }
+            }
+            else
+            {
+                elud--;
+            }
+
+
+            if (kuvatav == arvatav)
+            {
+
+                Console.WriteLine("win");
+                break;
+            }
+            if (elud == 0)
+            {
+
+                Console.Clear();
+                Console.Write(pildid[elud]);
+                Console.WriteLine("       " + arvatav + "\n      You got hung!");
+                break;
+            }
+
+        }
+        Console.ReadKey();
+    }
+
+}
+
+
+/*namespace ConsoleHangman
 {
     class Program
     {
@@ -12,75 +106,57 @@ namespace game
         {
             Console.WriteLine("You are playing Hangman!");
             Console.WriteLine("Quess the letters.. one by one");
-
-            Console.WriteLine("__________");
-
-
-            input = Console.ReadKey();
-            while (input==)
-
         }
+ 
+        private string[] words = new String[46] {"followed", "provided", "although", "happened", "question",
+            "American", "increase", "received", "anything", "industry", "religion", "building",
+            "interest", "remember", "business", "involved", "required", "children", "national",
+            "services", "complete", "organize", "southern","consider", "personal", "standard",
+            "continue", "planning", "strength", "couldn't", "position", "students","decision",
+            "possible", "suddenly", "directly", "pressure", "thinking", "district", "probably",
+            "together","economic", "problems", "training", "evidence", "programs" };
+ 
+        int word_index = 0;
+ 
+        if (word_index == i && word_index <= words.getLength)
+        {
+            return index
     }
-
-    class Word
+ 
+    public void Insert(int index, object word)
     {
-        private var words = new List<string> { "followed", "provided", "although", "happened", "question",
-        "American", "increase", "received", "anything", "industry", "religion", "building", "interest", "remember",
-        "business", "involved", "required", "children", "national", "services", "complete", "organize", "southern",
-        "consider", "personal", "standard", "continue", "planning", "strength", "couldn't", "position", "students",
-        "decision", "possible", "suddenly", "directly", "pressure", "thinking", "district", "probably", "together",
-        "economic", "problems", "training", "evidence", "programs" };
-
         Random random = new Random();
-
-        public Word new_Word()
-        {
-            var choosen_word = new int(random.Next(0, Word.words(Count);
-
-        }
-
+        var word = random.Next(0, words.Count());
+ 
+        IndexOf(random_word);
+        Count.random_word
+ 
+            Console.WriteLine("__________");
+        Console.WriteLine("Your letter: ");
+ 
+        var input_letter = Console.Read();
+        int input_count = 0;
+ 
     }
-
-    class Letter
+ 
+    public Word new_Word()
     {
-        private int letter;
-
-        private Letter[] letter_id = new letter[32];
-
-        //public Alphabet(number)
-        //{
-        //    int letter_id;
-        //   letter_id = 0;
-        //    letter_id++;
-        //}
-
-        public string[] letters = new String[32] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+        var choosen_word = new int(random.Next(0, Word.words(Count);
+    }
+ 
+    private int letter;
+ 
+    private Letter[] letter_id = new letter[32];
+ 
+    public string[] letters = new String[32] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
             "M", "N", "O", "P", "Q", "R", "S", "Š", "Z", "Ž", "T", "U", "V", "W", "Õ", "Ä", "Ö", "Ü", "X", "Y" };
-
-        letters get.. setkjsagkjn
-        int letter_id = 0;
-        letter_id++;
-    }
-       
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        bool m2ng_kestab = true;
-
-        vali v2lja s]na;
-            mitu t2hte s]nas on?;
-
-            Console.WriteLine("Arva ära sõna, milles on n arv tähti");
-            Console.Write("sisesta t2ht: ");
-
-            Console.WriteLine("Seda t2hte s]nas ei ole");
-            display word with input letters and *;
-            count arvatud_t2hed;
-            Console.WriteLine("Arvasid 5 korda valesti. M2ng on l2bi");
-            Console.WriteLine("Arvasid 2ra, s]na on {arvatuds]na}.");
-            Console.WriteLine("If you want to play again press [y]es..");
-
-        }
-    }
+ 
+    letters get..setkjsagkjn
+ 
+ 
+    int letter_id = 0;
+    letter_id++;
+          Console.WriteLine("If you want to play again press [y]es..");
 }
+    }
+    }*/
