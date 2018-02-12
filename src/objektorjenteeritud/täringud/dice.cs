6637fd8102b7cba374a7b4f721553b2e0ceea313
mrdks27 <33267841+mrdks00@users.noreply.github.com>
2018-02-12 16:44:35 +0200

    y

---
+++ src/objektorjenteeritud/täringud/dice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace täringud
+{
+    class Dice
+    {
+        // [access modifier] [return type] [nimi] [(parameters)]
+        public int Roll(int sides)
+        {
+            var random = new Random();
+
+            return random.Next(1, sides + 1);
+        }
+
+    }
+}
