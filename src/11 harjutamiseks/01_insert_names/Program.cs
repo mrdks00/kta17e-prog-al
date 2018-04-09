d269f5134a49d676d335889def71401974197b51
mrdks27 <33267841+mrdks00@users.noreply.github.com>
2018-04-09 03:52:09 +0300

    nimed.. nimekiri, timestamps

---
+++ src/11 harjutamiseks/01_insert_names/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_insert_names
+{
+    //Luua programm mis küsib inimeste nimesi niikaua kuni sisestatakse erimärk „-1“. 
+    //Iga sisestatud nimi lisatakse massiivi mis kuvatakse kasutajale programmi lõpus välja.
+    //Kindlasti tuleb veenduda, et esimene täht oleks suur.Selle võid lahendada nii, 
+    //et palud kasutajal sisestada sisendi õigel kujul 
+    //või siis vaikimisi teed programmis esimese tähe alati suureks.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insert names or [-1] to see all names");
+            Console.WriteLine();
+
+            List<string> Nimekiri = new List<string>();
+            string sisestus;
+
+            do
+            {
+                Console.Write("> ");
+
+                sisestus = Console.ReadLine();
+
+                if (sisestus == "-1")
+                {
+                    break;
+                }
+
+                Nimekiri.Add(CapitalizeFirstLetter(sisestus));
+
+            } while (true);
+
+            string sisestatudNimed = string.Join(", ", Nimekiri.ToArray());
+
+            Console.WriteLine();
+
+            Console.WriteLine($"all: {sisestatudNimed}");
+
+            Console.ReadLine();
+        }
+
+        static string CapitalizeFirstLetter(string nimi) //meetod(tegelt on see ka funktsioon, lihtsalt eriline nagu ruut) 
+        {
+            if (nimi.Length == 1)
+                return nimi.ToUpper();
+            return nimi.Remove(1).ToUpper() + nimi.Substring(1);
+        }
+    }
+}
+
---
+++ src/11 harjutamiseks/timestamps/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timestamps
+{
+    //Proramm küsib kasutaja käest kolme sisendit: 
+    //a) minimaalne aasta arv, 
+    //b) maksimaalne aasta arv, 
+    //c) suvaliselt genereeritavate andmete hulka.
+    //Vastavalt sisestatud andmetele genereeritakse suvalislet etteantud vahemikus ja hulgal timestamp’e.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            DateTime GetRandomDate(DateTime esimene, DateTime teine) //funktsioon
+            {
+                int vahe_days = (teine - esimene).Days;
+
+                return esimene.AddDays(random.NextDouble() * vahe_days);
+            }
+
+            Console.WriteLine("random timestamps generator");
+            Console.WriteLine();
+
+            Console.Write("min aasta: ");
+            int min_a = int.Parse(Console.ReadLine());
+
+            Console.Write("max aasta: ");
+            int max_a = int.Parse(Console.ReadLine());
+
+            Console.Write("hulk: ");
+            int hulk = int.Parse(Console.ReadLine());
+            
+            int a_vahe = max_a - min_a;
+
+            DateTime esimene_a = new DateTime(min_a, 1, 1);
+            DateTime teine_a = new DateTime(max_a, 12, 31);
+
+            Console.WriteLine("timestamps: ");
+
+            do
+            {
+                Console.WriteLine(GetRandomDate(esimene_a, teine_a));
+                hulk--;
+            }
+            while (hulk > 0);
+
+            Console.ReadLine();
+
+        }
+    }
+}
